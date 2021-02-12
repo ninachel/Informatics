19b0a21dafedb55c1d1dfb6600b5bff3d19efb91
ninachel <nccccc17@gmail.com>
2021-02-12 12:55:04 +0300

    -

--- hw1/h.py
+++ hw1/h.py
@@ -1,10 +1,13 @@
 x = int(input())
 n = int(input())
+lst = [[0] * n] * n
 for i in range(n):
+    lst[i] = list(map(int, input().split()))
+
+for j in range(n):
     in_col = False
-    for j in range(n):
-        cur = int(input())
-        if cur == x:
+    for i in range(n):
+        if lst[i][j] == x:
             in_col = True
     if in_col:
         print("YES")
