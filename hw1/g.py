981d75f79c55d88535baabea0da9dcf0ac918e52
ninachel <nccccc17@gmail.com>
2021-02-12 12:37:01 +0300

    -

--- hw1/g.py
+++ hw1/g.py
@@ -1,4 +1,14 @@
 n = int(input())
 lst = list(map(int, input().split()))
-lst.sort()
-print(lst[0], lst[1])
+
+imin_first = 0
+for i in range(n):
+    if lst[i] < lst[imin_first]:
+        imin_first = i
+
+imin_second = 0
+for i in range(n):
+    if lst[i] < lst[imin_second] and i != imin_first:
+        imin_second = i
+
+print(lst[imin_first], lst[imin_second])
