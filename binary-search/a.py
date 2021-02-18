7681b84226a94c24fdc416387aa3a70e2e703d81
ninachel <nccccc17@gmail.com>
2021-02-18 22:36:12 +0300

    -

--- binary-search/a.py
+++ binary-search/a.py
@@ -1,5 +1,22 @@
 def closest_num(lst, key):
-    pass
+    left = 0
+    right = len(lst)
+    lower_bound_index = -1
+    while left < right:
+        mid = (left + right) // 2
+        if lst[mid] < key:
+            lower_bound_index = mid
+            left = mid + 1
+        else:
+            right = mid
+
+    if lower_bound_index == -1:
+        return lst[-1]
+
+    if abs(key - lst[lower_bound_index]) > abs(key - lst[lower_bound_index - 1]):
+        return lst[lower_bound_index - 1]
+
+    return lst[lower_bound_index]
 
 
 n, k = map(int, input().split())
